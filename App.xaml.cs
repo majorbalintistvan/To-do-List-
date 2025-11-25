05360bf57c599677cd10e44f639c554aa79990e5
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2025-11-25 18:35:38 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace To_do_list
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
